650b2fcc1510ddbfc6c1c7bbb2229b10ba2e302b
Michael Connor <mike@sparkgeo.com>
2016-06-17 09:10:03 -0700

    Remove comments from controller.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -55,21 +55,3 @@ angular.module('starter.controllers', [])
   .controller('PlaylistCtrl', function ($scope, $stateParams) {
 
   });
-
-  // .controller('ExploreCtrl', function ($scope) {
-  //   $scope.maps = [
-  //     {title: 'Otway', id: 1},
-  //     {title: 'Pidherny', id: 2},
-  //     {title: 'UNBC', id: 3},
-  //     {title: 'Moores Meadow', id: 4},
-  //     {title: 'Cottonwood Park', id: 5}
-  //   ]
-  // })
-  //
-  // .controller('MapCtrl', function ($scope) {
-  //   //Create Map...
-  //   var map = new L.Map('cartodb-map', {
-  //     center: [0,0],
-  //     zoom: 2
-  //   })
-  // });
