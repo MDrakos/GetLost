d3a97fa5d97f4f348e48d3e262ef5daa8f319398
Hals <qkmaxware@gmail.com>
2016-06-21 17:51:49 -0700

    Contact us page

--- www/js/app.js
+++ www/js/app.js
@@ -86,8 +86,18 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         controller: 'PlaylistCtrl'
       }
     }
-  });
+  })
 
+	.state('app.contact',{
+		url: '/contact',
+		views:{
+			'menuContent':{
+				templateUrl: 'templates/contactus.html',
+				controller: 'ContactCtrl'
+			}
+		}
+	});
+	
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/start');
 });
