0dee930107f2dc22bcb66fc9f8098a915234ad31
Michael Connor <mike@sparkgeo.com>
2016-06-23 15:12:45 -0700

    Refactored to return promise.

--- www/js/cartoDBFactory.js
+++ www/js/cartoDBFactory.js
@@ -1,17 +1,35 @@
 angular.module('starter.factories', [])
+
 // Services for fetching data from CartoDB
   .service('MapService', function () {
-    
-    // Get the trail data from cartoDB
-    this.getTrail = function(layerName, trailName) {
-      sql = new cartodb.SQL({user: 'sparkgeo', format: 'geojson'});
-      sql.execute("SELECT * FROM " + layerName + " WHERE name='" + trailName + "'")
-        .done(function (data) {
-          return data;
-        })
-        .error(function (errors) {
-          console.log("fetch trail errors:" + errors);
-        });
-    }
-    
+
+    var sql = new cartodb.SQL({user: 'sparkgeo', format: 'geojson'});
+
+    return {
+      // Get the data for trail
+      getTrail: function (layerName, trailID) {
+
+        var sqlString = "SELECT * FROM {{layerName}} WHERE cartodb_id={{trailID}}";
+
+        var args = {
+          layerName: layerName,
+          trailID: trailID
+        };
+
+        return sql.execute(sqlString, args);
+      },
+
+      // Get a list of trails for a layer
+      listTrails: function (layerName) {
+
+        var sqlString = "SELECT * FROM {{layerName}}";
+
+        var args = {
+          layerName: layerName
+        };
+
+        return sql.execute(sqlString, args);
+      }
+    };
+
   });
