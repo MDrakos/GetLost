b97e9244249fc212b0512fac6315c16be74e89cb
Michael Connor <mike@sparkgeo.com>
2016-07-03 09:43:32 -0700

    Add mapService calls adding trail data to maps.

--- www/js/mapController.js
+++ www/js/mapController.js
@@ -1,16 +1,19 @@
 angular.module('starter.controllers')
 
-  .controller('ExploreCtrl', function ($scope) {
-    $scope.maps = [
-      {title: 'Otway', id: 1},
-      {title: 'Pidherny', id: 2},
-      {title: 'UNBC', id: 3},
-      {title: 'Moores Meadow', id: 4},
-      {title: 'Cottonwood Park', id: 5}
-    ]
+  .constant('SHORT_STYLE', {"color": "#FFE403", "weight": 5, "opacity": 0.65})
+
+  .service('mapReference', function () {
+    this.map = null;
+    this.layer = null;
   })
 
-  .controller('MapCtrl', function ($scope) {
+  .controller('ExploreCtrl', function ($scope, MapService) {
+    MapService.listTrails().done(function(data){
+      $scope.maps = data.features;
+    });
+  })
+
+  .controller('MapCtrl', function ($scope, $stateParams, MapService, SHORT_STYLE, mapReference) {
 
     var OpenTopoMap = L.tileLayer('http://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
       maxZoom: 17,
@@ -36,28 +39,31 @@ angular.module('starter.controllers')
       maxZoom: 11
     });
 
-    // initiate leaflet map
-    var map = new L.Map('cartodbMap', {
-      center: [53.966, -122.88],
-      zoom: 14,
-      layers: [Esri_WorldImagery]
+    var currentTileLayer = Esri_WorldTopoMap; // Change this to change the tileLayer
+
+    // Initialize leaflet map
+    MapService.getTrail($stateParams.mapId).done(function(data) {
+      // Create new layer
+      mapReference.layer = L.geoJson(data, {style: SHORT_STYLE});
+
+      if (mapReference.map !== null){
+        // Remove the layer, add a new one
+        console.log('Map Exists');
+        mapReference.map.removeLayer(mapReference.layer);
+        mapReference.map.addLayer(mapReference.layer);
+      } else {
+        // Create new map
+        console.log('New Map');
+        mapReference.map = new L.Map('cartodbMap', {
+          dragging: true,
+          layers: [mapReference.layer, currentTileLayer]
+        });
+      }
+
+      // Fit map to trail bounds
+      mapReference.map.fitBounds(mapReference.layer.getBounds());
+
     });
 
-    // // create the tile layer with correct attribution
-    // var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-    // // var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-    // var osmAttrib='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
-    //
-    // L.tileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib}).addTo(map);
-
-    var layerUrl = 'https://sparkgeo.cartodb.com/api/v2/viz/5d02bb8c-0a03-11e5-a7d9-0e018d66dc29/viz.json';
-
-    cartodb.createLayer(map, layerUrl)
-      .addTo(map)
-      .on('done', function(layer) {
-
-      }).on('error', function() {
-      //log the error
-    });
 
   });
