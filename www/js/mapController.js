8dd65a15e2d400e4a72323bb6263638e294ad1aa
braem <bbraess@gmail.com>
2016-07-07 13:14:47 -0700

    Commented out ExploreCtrl so as to prevent duplicate ExploreCtrl's
    In the future only one will remain

--- www/js/mapController.js
+++ www/js/mapController.js
@@ -1,12 +1,12 @@
 angular.module('starter.controllers')
-
+/*
   .controller('ExploreCtrl', function ($scope, MapService) {
     MapService.listTrails().done(function(data){
       var features = data.features;
       $scope.maps = features;
     });
   })
-
+*/
   .controller('MapCtrl', function ($scope, MapService) {
     MapService.getTrail($stateParams.playlistId).done(function(data){
       console.log(data);
