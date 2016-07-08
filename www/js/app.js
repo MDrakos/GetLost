f16dfff0ec8ff8feb6f0b47dded1758262cd0f92
braem <bbraess@gmail.com>
2016-07-07 17:01:12 -0700

    Added new requires to app.js for starter.factories

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'jett.ionic.filter.bar',
-  'ti-segmented-control', 'ngMaterial'])
+  'ti-segmented-control', 'ngMaterial', 'starter.factories'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
